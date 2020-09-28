7eb7bac79580aea233b6be5fde40a50d656aee5c
qiruihua <qiruihua@itcast.cn>
2020-09-28 16:48:32 +0800

    判断用户名是否重复的实现

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.http import HttpResponse
 def log(request):
@@ -33,5 +33,6 @@ def log(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('log/', log),
+    #导入 users子应用的路由
+    path('',include('apps.users.urls')),
 ]
