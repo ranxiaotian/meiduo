7e82fe3bd08b2bdc66c6e429cedff8a4ed2a4baf
qiruihua <qiruihua@itcast.cn>
2020-09-30 09:58:22 +0800

    注册功能的实现

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -43,3 +43,80 @@ class UsernameCountView(View):
         count=User.objects.filter(username=username).count()
         # 3.  返回响应
         return JsonResponse({'code':0,'count':count,'errmsg':'ok'})
+
+"""
+我们不相信前端提交的任何数据！！！！
+
+前端：     当用户输入 用户名，密码，确认密码，手机号，是否同意协议之后，会点击注册按钮
+            前端会发送axios请求
+
+后端：
+    请求：             接收请求（JSON）。获取数据
+    业务逻辑：          验证数据。数据入库
+    响应：             JSON {'code':0,'errmsg':'ok'}
+                     响应码 0 表示成功 400表示失败
+    
+    路由：     POST    register/
+    
+    步骤：
+    
+        1. 接收请求（POST------JSON）
+        2. 获取数据
+        3. 验证数据
+            3.1 用户名，密码，确认密码，手机号，是否同意协议 都要有
+            3.2 用户名满足规则，用户名不能重复
+            3.3 密码满足规则
+            3.4 确认密码和密码要一致
+            3.5 手机号满足规则，手机号也不能重复
+            3.6 需要同意协议
+        4. 数据入库
+        5. 返回响应
+
+
+"""
+import json
+
+class RegisterView(View):
+
+    def post(self,request):
+        # 1. 接收请求（POST------JSON）
+        body_bytes=request.body
+        body_str=body_bytes.decode()
+        body_dict=json.loads(body_str)
+
+        # 2. 获取数据
+        username=body_dict.get('username')
+        password=body_dict.get('password')
+        password2=body_dict.get('password2')
+        mobile=body_dict.get('mobile')
+        allow=body_dict.get('allow')
+
+        # 3. 验证数据
+        #     3.1 用户名，密码，确认密码，手机号，是否同意协议 都要有
+        # all([xxx,xxx,xxx])
+        # all里的元素 只要是 None,False
+        # all 就返回False，否则返回True
+        if not all([username,password,password2,mobile,allow]):
+            return JsonResponse({'code':400,'errmsg':'参数不全'})
+        #     3.2 用户名满足规则，用户名不能重复
+        if not re.match('[a-zA-Z_-]{5,20}',username):
+            return JsonResponse({'code': 400, 'errmsg': '用户名不满足规则'})
+        #     3.3 密码满足规则
+        #     3.4 确认密码和密码要一致
+        #     3.5 手机号满足规则，手机号也不能重复
+        #     3.6 需要同意协议
+        # 4. 数据入库
+        # user=User(username=username,password=password,moble=mobile)
+        # user.save()
+
+        # User.objects.create(username=username,password=password,mobile=mobile)
+
+        #以上2中方式，都是可以数据入库的
+        # 但是 有一个问题 密码没有加密
+
+        # 密码就加密
+        user=User.objects.create_user(username=username,password=password,mobile=mobile)
+
+        # 5. 返回响应
+        return JsonResponse({'code':0,'errmsg':'ok'})
+
