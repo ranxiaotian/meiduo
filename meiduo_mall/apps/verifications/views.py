d5cdd6cfdc6d323d22deff2b579d5ef691469d94
qiruihua <qiruihua@itcast.cn>
2020-10-07 10:02:42 +0800

    避免频繁发送短信

--- meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/apps/verifications/views.py
@@ -122,11 +122,21 @@ class SmsCodeView(View):
         # 3.3 对比
         if redis_image_code.decode().lower() != image_code.lower():
             return JsonResponse({'code':400,'errmsg':'图片验证码错误'})
+
+        # 提取发送短信的标记，看看有没有
+        send_flag=redis_cli.get('send_flag_%s'%mobile)
+
+        if send_flag is not None:
+            return JsonResponse({'code':400,'errmsg':'不要频繁发送短信'})
+
         # 4. 生成短信验证码
         from  random import randint
         sms_code= '%06d'%randint(0,999999)
         # 5. 保存短信验证码
         redis_cli.setex(mobile,300,sms_code)
+        # 添加一个发送标记.有效期 60秒 内容是什么都可以
+        redis_cli.setex('send_flag_%s'%mobile,60,1)
+
         # 6. 发送短信验证码
         from libs.yuntongxun.sms import CCP
         CCP().send_template_sms(mobile,[sms_code,5],1)
