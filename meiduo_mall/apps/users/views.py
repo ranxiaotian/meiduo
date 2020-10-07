1baa1634fdbe9648333146801a3f04348c47f4e8
qiruihua <qiruihua@itcast.cn>
2020-10-07 17:14:06 +0800

    退出功能

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -218,3 +218,26 @@ class LoginView(View):
         response.set_cookie('username',username)
 
         return response
+
+"""
+前端：
+    当用户点击退出按钮的时候，前端发送一个axios delete请求
+
+后端：
+    请求
+    业务逻辑        退出
+    响应      返回JSON数据
+
+"""
+from django.contrib.auth import logout
+class LogoutView(View):
+
+    def get(self,request):
+        # 1. 删除session信息
+        logout(request)
+
+        response = JsonResponse({'code':0,'errmsg':'ok'})
+        # 2. 删除cookie信息，为什么要是删除呢？ 因为前端是根据cookie信息来判断用户是否登录的
+        response.delete_cookie('username')
+
+        return response
