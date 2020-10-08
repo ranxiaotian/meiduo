0d7aa45ae5fac20947c04738a52bd139ec7d5322
qiruihua <qiruihua@itcast.cn>
2020-10-08 10:49:46 +0800

    生成QQ登录跳转的链接

--- meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/apps/oauth/views.py
@@ -38,3 +38,43 @@ from django.shortcuts import render
 生成用户绑定链接 ----------》获取code   ------------》获取token ------------》获取openid --------》保存openid
 
 """
+
+
+"""
+生成用户绑定链接
+
+前端： 当用户点击QQ登录图标的时候，前端应该发送一个axios(Ajax)请求
+
+后端：
+    请求
+    业务逻辑        调用QQLoginTool 生成跳转链接
+    响应            返回跳转链接 {"code":0,"qq_login_url":"http://xxx"}
+    路由          GET   qq/authorization/
+    步骤      
+            1. 生成 QQLoginTool 实例对象
+            2. 调用对象的方法生成跳转链接
+            3. 返回响应
+            
+404 路由不匹配
+405 方法不被允许（你没有实现请求对应的方法）
+"""
+from django.views import View
+from QQLoginTool.QQtool import OAuthQQ
+from meiduo_mall import settings
+from django.http import JsonResponse
+class QQLoginURLView(View):
+
+    def get(self,request):
+        # 1. 生成 QQLoginTool 实例对象
+        # client_id=None,               appid
+        # client_secret=None,           appsecret
+        # redirect_uri=None,            用户同意登录之后，跳转的页面
+        # state=None                    不知道什么意思，随便写。等出了问题再分析问题
+        qq=OAuthQQ(client_id=settings.QQ_CLIENT_ID,
+                   client_secret=settings.QQ_CLIENT_SECRET,
+                   redirect_uri=settings.QQ_REDIRECT_URI,
+                   state='xxxxx')
+        # 2. 调用对象的方法生成跳转链接
+        qq_login_url=qq.get_qq_url()
+        # 3. 返回响应
+        return JsonResponse({'code':0,'errmsg':'ok','login_url':qq_login_url})
