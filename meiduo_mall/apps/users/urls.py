2f20cb2c415e9e0a03893a9aebcf2b887d0ad03c
qiruihua <qiruihua@itcast.cn>
2020-10-10 11:33:50 +0800

    实现邮件激活的功能

--- meiduo_mall/apps/users/urls.py
+++ meiduo_mall/apps/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from apps.users.views import UsernameCountView,RegisterView,LoginView,LogoutView
-from apps.users.views import CenterView,EmailView
+from apps.users.views import CenterView,EmailView,EmailVerifyView
 urlpatterns = [
     #判断用户名是否重复
     path('usernames/<username:username>/count/',UsernameCountView.as_view()),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('logout/',LogoutView.as_view()),
     path('info/',CenterView.as_view()),
     path('emails/',EmailView.as_view()),
+    path('emails/verification/',EmailVerifyView.as_view()),
 
 ]
--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -384,3 +384,51 @@ django 项目
     
 3. 调用  send_mail 方法
 """
+
+
+
+"""
+需求（知道我们要干什么？？？）：
+    激活用户的邮件
+前端(用户干了什么，传递了什么参数)：
+        用户会点击那个激活连接。那个激活连接携带了token
+后端：
+    请求：         接收请求，获取参数，验证参数
+    业务逻辑：       user_id, 根据用户id查询数据，修改数据
+    响应：         返回响应JSON
+    
+    路由：         PUT     emails/verification/  说明： token并没有在body里
+    步骤：
+        
+        1. 接收请求
+        2. 获取参数
+        3. 验证参数
+        4. 获取user_id
+        5. 根据用户id查询数据
+        6. 修改数据
+        7. 返回响应JSON
+
+"""
+
+class EmailVerifyView(View):
+
+    def put(self,request):
+        # 1. 接收请求
+        params=request.GET
+        # 2. 获取参数
+        token=params.get('token')
+        # 3. 验证参数
+        if token is None:
+            return JsonResponse({'code':400,'errmsg':'参数缺失'})
+        # 4. 获取user_id
+        from apps.users.utils import check_verify_token
+        user_id=check_verify_token(token)
+        if user_id is None:
+            return JsonResponse({'code':400,'errmsg':'参数错误'})
+        # 5. 根据用户id查询数据
+        user=User.objects.get(id=user_id)
+        # 6. 修改数据
+        user.email_active=True
+        user.save()
+        # 7. 返回响应JSON
+        return JsonResponse({'code':0,'errmsg':'ok'})
