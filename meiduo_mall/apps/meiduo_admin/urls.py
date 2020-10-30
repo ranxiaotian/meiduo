3a4cf8aef32dc44abf6388d109654d457ed8f0a2
qiruihua <qiruihua@itcast.cn>
2020-10-30 16:14:22 +0800

    实现is_staff用户登录

--- meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/apps/meiduo_admin/urls.py
@@ -1,6 +1,7 @@
-from rest_framework_jwt.views import obtain_jwt_token
+# from rest_framework_jwt.views import obtain_jwt_token
 from django.urls import path
-
+from apps.meiduo_admin.user import meiduo_token
 urlpatterns = [
-    path('authorizations/',obtain_jwt_token),
+    # path('authorizations/',obtain_jwt_token),
+    path('authorizations/',meiduo_token),
 ]
