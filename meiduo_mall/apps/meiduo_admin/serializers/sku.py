89b65324bffee1ec7a097002e1ea274c0fb453f3
qiruihua <qiruihua@itcast.cn>
2020-11-02 10:06:47 +0800

    sku新增的前2个问题(spu和spu_id的问题)

--- meiduo_mall/apps/meiduo_admin/serializers/sku.py
+++ meiduo_mall/apps/meiduo_admin/serializers/sku.py
@@ -3,6 +3,45 @@ from rest_framework import serializers
 
 class SKUModelSerializer(serializers.ModelSerializer):
 
+    """
+    前端传递的数据形式是
+    caption: "12不想13香"
+    category_id: 115            ~~~~~~
+    cost_price: "1"
+    is_launched: "true"
+    market_price: "11"
+    name: "13香"
+    price: "1"
+    specs: [{spec_id: "4", option_id: 8}, {spec_id: "5", option_id: 11}]
+    spu_id: 2                   ~~~~~~~
+    stock: "1"
+
+    Q1. 外键中 spu和category的数据  前端是以 category_id 和 spu_id 的形式传递的 所以我们的序列化器 要改变
+        spu_id=serializers.IntegerField()
+        category_id=serializers.IntegerField()
+
+    Q2. 我们 通过 添加 spu_id 和 category_id 能接收前端的数据,
+        但是并没有改变 系统自动生成的  spu和category 这2个字段必传的选项
+        如何去解决呢???
+        ① 改为 required=False
+        ② 为了配合查询数据的展示 我们 重写 spu 和category 这2个字段
+
+        spu=serializers.StringRelatedField()
+        category=serializers.StringRelatedField()
+
+        新的疑问!!!  默认的字段是 required=True
+
+        StringRelatedField 本质是什么???
+        本质是 获取 关联模型的 __str__ 里的数据
+
+
+    """
+    spu_id=serializers.IntegerField()
+    category_id=serializers.IntegerField()
+
+    spu=serializers.StringRelatedField(required=False)
+    category=serializers.StringRelatedField()
+
     class Meta:
         model=SKU
         fields='__all__'
